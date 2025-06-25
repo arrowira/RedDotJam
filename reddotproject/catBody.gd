4eafbbebe56134b1e45ea88fa75845efe3758fce
arrowira <41136800+arrowira@users.noreply.github.com>
2025-06-25 14:03:33 -0700

    pointer visibility
    LFIJGURHGIUHROGUHODRIGH

--- reddotproject/catBody.gd
+++ reddotproject/catBody.gd
@@ -5,27 +5,35 @@ extends CharacterBody2D
 const jumpVelocity = -400.0
 @export var speed = 3
 var dir = 0
-
+var seesPointer = false
+var redDotVisible = true
 func _physics_process(delta: float) -> void:
+	$vision.target_position = to_local(get_global_mouse_position())
+	if $vision.is_colliding():
+		redDotVisible = false
+		print($vision.get_collider())
+	else:
+		print("visible")
+		redDotVisible = true
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	if redDotVisible:
+		#direction cat moves in
+		dir = get_global_mouse_position().x-position.x
+		if dir <0:
+			dir = -1
+		else:
+			dir = 1
+			
 		
-	#direction cat moves in
-	dir = get_global_mouse_position().x-position.x
-	if dir <0:
-		dir = -1
-	else:
-		dir = 1
-		
-	
-	#cat movement
-	velocity.x+=dir*delta*speed
-	#jump checks
-	if abs(get_global_mouse_position().y-position.y)>60 and get_global_mouse_position().y<position.y and is_on_floor():
-		#if in jump range
-		if abs(get_global_mouse_position().x-position.x)<100:
-			velocity.y = jumpVelocity
+		#cat movement
+		velocity.x+=dir*delta*speed
+		#jump checks
+		if abs(get_global_mouse_position().y-position.y)>60 and get_global_mouse_position().y<position.y and is_on_floor():
+			#if in jump range
+			if abs(get_global_mouse_position().x-position.x)<150:
+				velocity.y = jumpVelocity
 
 		
 		
