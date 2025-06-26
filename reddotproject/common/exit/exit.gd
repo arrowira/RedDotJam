115b793ce52357e3956c83d4928854b215ecc4f1
La-ryssa <mgamersilva9@gmail.com>
2025-06-26 01:30:09 -0300

    transition repeating fix

--- reddotproject/common/exit/exit.gd
+++ reddotproject/common/exit/exit.gd
@@ -1,13 +1,21 @@
 extends Area2D
 
+var was_transition_triggered = false
+
+
 func _ready():
     
     # Connect signal programmatically (optional if you use the editor)
     connect("body_entered", Callable(self, "_on_body_entered"))
 
+
 func _on_body_entered(body):
+    if was_transition_triggered:
+        return
     
     if body.name == "Cat":
+        was_transition_triggered = true
+        
         print("endLvl")
         
         #body.queue_free()
