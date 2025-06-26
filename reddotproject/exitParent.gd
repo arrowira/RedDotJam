7d59d5b4a5af53d2d65abd10bab76abfe3d507c5
arrowira <41136800+arrowira@users.noreply.github.com>
2025-06-25 19:51:23 -0700

    simple level transition

---
+++ reddotproject/exitParent.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var lvlName = ""
+@export var lvlPath = "res://" + lvlName + ".tscn"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	lvlPath = "res://" + lvlName + ".tscn"
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
