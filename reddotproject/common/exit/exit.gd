b9358549033227bfc1811a72cc547c865424cc9a
arrowira <41136800+arrowira@users.noreply.github.com>
2025-06-29 00:16:11 -0700

    up to level 7

--- reddotproject/common/exit/exit.gd
+++ reddotproject/common/exit/exit.gd
@@ -4,31 +4,31 @@ var was_transition_triggered = false
 
 
 func _ready():
-    
-    # Connect signal programmatically (optional if you use the editor)
-    connect("body_entered", Callable(self, "_on_body_entered"))
+	
+	# Connect signal programmatically (optional if you use the editor)
+	connect("body_entered", Callable(self, "_on_body_entered"))
 
 
 func _on_body_entered(body):
-    if was_transition_triggered:
-        return
-    
-    if body.name == "Cat":
-        was_transition_triggered = true
-        
-        print("endLvl")
-        
-        #body.queue_free()
-        
-        var exit = get_parent()
-        exit.ui.fade_into_black()
-        
-        $Timer.start()
-        
-        
-        
-    
+	if was_transition_triggered:
+		return
+	
+	if body.name == "Cat":
+		was_transition_triggered = true
+		
+		print("endLvl")
+		
+		#body.queue_free()
+		
+		var exit = get_parent()
+		exit.ui.fade_into_black()
+		
+		$Timer.start()
+		
+		
+		
+	
 
 
 func _on_timer_timeout() -> void:
-    get_tree().change_scene_to_file(get_parent().lvlPath)
+	get_tree().change_scene_to_file(get_parent().lvlPath)
